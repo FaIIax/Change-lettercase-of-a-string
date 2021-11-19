a6311d19f83febb49a13523eab02433f18be9541
FaIIax <81348662+FaIIax@users.noreply.github.com>
2021-11-19 22:36:19 +0530

    Add files via upload

---
+++ Change letter case.cpp
@@ -0,0 +1,28 @@
+/*3. Write a program that inputs a line of text into char array s[ 100 ]. Output the line in 
+uppercase letters and in lowercase letters. */
+
+/*#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+  char s[100];
+
+  cout<<"Enter a text: ";
+  cin.getline(s, 100);
+
+  cout<<"\nText in uppercase: "; 
+  for(int i=0; i<strlen(s); i++){
+    s[i]=toupper(s[i]);
+    cout<<s[i];
+  }
+
+  cout<<"\nText in lowercase: ";
+  for(int j=0; j<strlen(s); j++){
+    s[j]=tolower(s[j]);
+    cout<<s[j];
+  }
+  
+}*/
